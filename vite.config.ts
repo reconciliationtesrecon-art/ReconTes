3fc8365c390ae036d396683ebb461f931883452a
reconciliationtesrecon-art <reconciliationtesrecon@gmail.com>
2025-09-26 13:01:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ReconTes/",   // HARUS sama persis dengan nama repo (case-sensitive)
   plugins: [react()],
-});
+  // penting untuk GitHub Pages
+  base: '/ReconTes/', 
+})
